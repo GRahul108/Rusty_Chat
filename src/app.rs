668e221685ab376b7a817afbba8aed406af6641c
GRahul108 <rahulchand810@gmail.com>
2023-11-03 14:42:56 -0400

    Fixed Errors to merge

--- src/app.rs
+++ src/app.rs
@@ -6,6 +6,28 @@ use leptos_router::*;
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+    let (conversation, set_conversation) = create_signal(Conversation::new());
+    let send = create_action(move |new_message: &String| {
+        let user_message = Message {
+            text: new_message.clone(),
+            user: true,
+        };
+        set_conversation.update(move |c| {
+            c.messages.push(user_message);
+        });
+
+        let client2 = client.clone();
+        let msg = new_message.to_string();
+        async move {
+            client2
+                .borrow_mut()
+                .as_mut()
+                .unwrap()
+                .send(Txt(msg.to_string()))
+                .await
+                .map_err(|_| ServerFnError::ServerError("WebSocket issue".to_string()))
+        }
+    });
   
     view! {
         // injects a stylesheet into the document <head>
