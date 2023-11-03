a10600568243c04db2b6ff2e4d179d654999cf5f
GRahul108 <rahulchand810@gmail.com>
2023-11-03 17:02:31 -0400

    Modified type area component

--- src/app-components/type_area.rs
+++ src/app-components/type_area.rs
@@ -4,6 +4,14 @@ const TYPE_AREA_CLASS: &str = "h-24 w-full fixed bottom-0 flex justify-center it
 const TYPE_AREA_CLASS_LIGHT: &str = "bg-white border-gray-300";
 const TYPE_AREA_CLASS_DARK: &str = "bg-zinc-900 border-zinc-700";
 
+const TEXT_AREA_CLASS: &str = "w-2/3 p-4 border rounded-full input-field";
+const TEXT_AREA_CLASS_LIGHT: &str = "border-gray-300 text-black";
+const TEXT_AREA_CLASS_DARK: &str = "bg-zinc-700 border-zinc-700 text-white";
+
+const BUTTON_CLASS: &str = "h-full p-4 rounded-full cursor-pointer" ;
+const BUTTON_CLASS_LIGHT: &str = "bg-blue-500 text-white";
+const BUTTON_CLASS_DARK: &str = "bg-green-700 text-white";
+
 #[component]
 pub fn TypeArea(send: Action<String, Result<(), ServerFnError>>) -> impl IntoView {
     let dark_mode = use_context::<ReadSignal<bool>>().expect("should be able to get dark mode state");
@@ -15,7 +23,20 @@ pub fn TypeArea(send: Action<String, Result<(), ServerFnError>>) -> impl IntoVie
         format!("{TYPE_AREA_CLASS} {TYPE_AREA_CLASS_LIGHT}")
       }
     });
-
+    let text_area_class = Signal::derive(move || {
+        if dark_mode.get() {
+          format!("{TEXT_AREA_CLASS} {TEXT_AREA_CLASS_DARK}")
+        } else {
+          format!("{TEXT_AREA_CLASS} {TEXT_AREA_CLASS_LIGHT}")
+        }
+      });
+      let button_class = Signal::derive(move || {
+        if dark_mode.get() {
+          format!("{BUTTON_CLASS} {BUTTON_CLASS_DARK}")
+        } else {
+          format!("{BUTTON_CLASS} {BUTTON_CLASS_LIGHT}")
+        }
+      });
     
 
     let input_ref = create_node_ref::<Input>();
@@ -28,7 +49,12 @@ pub fn TypeArea(send: Action<String, Result<(), ServerFnError>>) -> impl IntoVie
                 input.set_value("");
            }
            >
-                
+                <input class={text_area_class.get()} type="text" placeholder="Enter your prompt" node_ref=input_ref/>
+                <button class={button_class.get()} type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                    </svg>
+                </button>
            </form>
         </div>
     }
