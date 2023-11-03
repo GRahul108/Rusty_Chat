99fb8f25340788b4cc5609462781a61d74fc80a9
GRahul108 <rahulchand810@gmail.com>
2023-11-03 14:06:55 -0400

    Implemented Send action in type area

--- src/app.rs
+++ src/app.rs
@@ -6,6 +6,28 @@ use leptos_router::*;
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+    let (conversation, set_conversation) = create_signal(Conversation::new());
+    let send = create_action(move |new_message: &String| {
+        let user_message = Message {
+            text: new_message.clone(),
+            user: true,
+        };
+        set_conversation.update(move |c| {
+            c.messages.push(user_message);
+        });
+
+        let client2 = client.clone();
+        let msg = new_message.to_string();
+        async move {
+            client2
+                .borrow_mut()
+                .as_mut()
+                .unwrap()
+                .send(Txt(msg.to_string()))
+                .await
+                .map_err(|_| ServerFnError::ServerError("WebSocket issue".to_string()))
+        }
+    });
 
     view! {
         // injects a stylesheet into the document <head>
@@ -13,51 +35,12 @@ pub fn App() -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
 
         // sets the document title
-        <Title text="Welcome to Leptos"/>
+        <Title text="Rusty Chat"/>
+        <ChatArea/>
+        <TypeArea/>
 
-        // content for this welcome page
-        <Router>
-            <main>
-                <Routes>
-                    <Route path="" view=HomePage/>
-                    <Route path="/*any" view=NotFound/>
-                </Routes>
-            </main>
-        </Router>
+       >
     }
 }
 
-/// Renders the home page of your application.
-#[component]
-fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
-    view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
-    }
-}
-
-/// 404 - Not Found
-#[component]
-fn NotFound() -> impl IntoView {
-    // set an HTTP status code 404
-    // this is feature gated because it can only be done during
-    // initial server-side rendering
-    // if you navigate to the 404 page subsequently, the status
-    // code will not be set because there is not a new HTTP request
-    // to the server
-    #[cfg(feature = "ssr")]
-    {
-        // this can be done inline because it's synchronous
-        // if it were async, we'd use a server function
-        let resp = expect_context::<leptos_actix::ResponseOptions>();
-        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
-    }
-
-    view! {
-        <h1>"Not Found"</h1>
-    }
-}
